a2b44ba9ec6ee7e68f961456469b18bf9b8fa916
asobrado <asobrado@gmail.com>
2015-05-06 13:32:27 -0300

    se agrego archivo para generar lacarpeta cache

---
+++ crearDirectoriosOJS(1).sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir public; chmod 777 public;
+mkdir cache; chmod 777 cache; cd cache
+mkdir t_cache; chmod 777 t_cache
+mkdir t_compile; chmod 777 t_compile
+mkdir _db; chmod 777 _db
+
